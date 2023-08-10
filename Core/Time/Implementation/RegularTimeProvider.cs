066e04e55dad47fe67f041f7df65d93b543c18e2
Dmytrii Petrinko <dmytrii.petrinko@gmail.com>
2023-08-11 02:24:08 +0300

    Implemented custom timing

---
+++ Core/Time/Implementation/RegularTimeProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Time.Implementation
+{
+	internal class RegularTimeProvider : ITimeProvider
+	{
+		private readonly DateTime mStartTime;
+
+		public double SecondsSinceStart => (DateTime.UtcNow - mStartTime).TotalSeconds;
+
+		public RegularTimeProvider()
+		{
+			mStartTime = DateTime.UtcNow;
+		}
+
+		public Task Delay(int milliseconds, CancellationToken token = default)
+		{
+			return Task.Delay(milliseconds, token);
+		}
+
+		public Task Delay(TimeSpan timeSpan, CancellationToken token = default)
+		{
+			return Task.Delay(timeSpan, token);
+		}
+	}
+}
---
+++ Core/Time/Time.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Time.Implementation;
+
+namespace Core.Time
+{
+	public static class Time
+	{
+		private static ITimeProvider sTimeProvider = new RegularTimeProvider();
+
+		public static double SecondsSinceStart => sTimeProvider.SecondsSinceStart;
+		public static Task Delay(int milliseconds, CancellationToken token = default)
+		{
+			return sTimeProvider.Delay(milliseconds, token);
+		}
+
+		public static Task Delay(TimeSpan timeSpan, CancellationToken token = default)
+		{
+			return sTimeProvider.Delay(timeSpan, token);
+		}
+
+		public static void RegisterTimeProvider(ITimeProvider timeProvider)
+		{
+			sTimeProvider = timeProvider;
+		}
+
+		public static void ResetTimeProvider()
+		{
+			sTimeProvider = new RegularTimeProvider();
+		}
+	}
+}
